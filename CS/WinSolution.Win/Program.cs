bc8f0291cadf3648914c0742fca46806a2b41675
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/WinSolution.Win/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Configuration;
+using System.Windows.Forms;
+using DevExpress.ExpressApp.Security;
+
+namespace WinSolution.Win {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            EditModelPermission.AlwaysGranted = System.Diagnostics.Debugger.IsAttached;
+            WinSolutionWindowsFormsApplication _application = new WinSolutionWindowsFormsApplication();
+            if (ConfigurationManager.ConnectionStrings["ConnectionString"] != null) {
+                _application.ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            }
+            try {
+                DevExpress.ExpressApp.Xpo.InMemoryDataStoreProvider.Register();
+                                _application.ConnectionString = DevExpress.ExpressApp.Xpo.InMemoryDataStoreProvider.ConnectionString;
+                _application.Setup();
+                _application.Start();
+            } catch (Exception e) {
+                _application.HandleException(e);
+            }
+        }
+    }
+}
